5a21cf53362e14818fae49217c6e3bd2a0bc6b36
Hazem ali <81050076+hazemali16@users.noreply.github.com>
2024-06-04 19:04:25 +0300

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,15 +5,107 @@ import { useEffect, useState } from 'react';
 const Portfolio = () => {
   const [projects, setProjects] = useState([])
   const [skills, setSkills] = useState([])
+  let desktopMenu = document.querySelector(".desktop-menu")
+  let mobileNavbar = document.querySelector(".mobile-navbar")
+  let checkPorfolio = document.querySelector(".check-porfolio")
+  let goUp = document.querySelector(".go-up")
+  let about = document.querySelector(".about")
+  let portfolio = document.querySelector(".portfolio")
+  let contact = document.querySelector(".contact")
+
   // let skills = new Set()
   useEffect(() => {
-    fetch("https://hazem1portfolio.pythonanywhere.com/projects-data/")
+    fetch("http://127.0.0.1:8000/projects-data/")
     .then((response) => response.json())
     .then((projects_result) => {
       setProjects(projects_result.results);
+      setTimeout(() => {
+        checkPorfolio.onclick = () => {
+          window.scrollTo({
+            top: portfolio.offsetTop,
+            left: 0,
+            behavior: "smooth", 
+          })
+        }
+        let scrollWraper = document.querySelector(".scroll-wraper")
+        let height = scrollWraper.getBoundingClientRect().height 
+        let speed = 0.05
+        let offset = 0
+        document.body.style.height = Math.floor(height) + "px"
+        function smoothScroll() {
+          offset += (window.scrollY - offset) * speed
+          var scroll = "translateY(-" + offset + "px) translateZ(0)";
+          scrollWraper.style.transform = scroll;
+          let callScroll = requestAnimationFrame(smoothScroll)
+        }
+        smoothScroll()
+        goUp.onclick = () => {
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth", 
+          })
+        }
+        desktopMenu.children[0].onclick = () => {
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth", 
+          })
+        }
+        desktopMenu.children[1].onclick = () => {
+          window.scrollTo({
+            top: about.offsetTop,
+            left: 0,
+            behavior: "smooth", 
+          })
+        }
+        desktopMenu.children[2].onclick = () => {
+          window.scrollTo({
+            top: portfolio.offsetTop,
+            left: 0,
+            behavior: "smooth", 
+          })
+        }
+        desktopMenu.children[3].onclick = () => {
+          window.scrollTo({
+            top: contact.offsetTop,
+            left: 0,
+            behavior: "smooth", 
+          })
+        }
+        mobileNavbar.children[0].onclick = () => {
+          window.scrollTo({
+            top: 0,
+            left: 0,
+            behavior: "smooth", 
+          })
+        }
+        mobileNavbar.children[1].onclick = () => {
+          window.scrollTo({
+            top: about.offsetTop,
+            left: 0,
+            behavior: "smooth", 
+          })
+        }
+            mobileNavbar.children[2].onclick = () => {
+          window.scrollTo({
+            top: portfolio.offsetTop,
+            left: 0,
+            behavior: "smooth", 
+          })
+        }
+        mobileNavbar.children[3].onclick = () => {
+          window.scrollTo({
+            top: contact.offsetTop,
+            left: 0,
+            behavior: "smooth", 
+          })
+        }
+      }, 1000);
     })
     .catch((error) => console.error(error));
-    fetch("https://hazem1portfolio.pythonanywhere.com/skills-data/")
+    fetch("http://127.0.0.1:8000/skills-data/")
     .then((response) => response.json())
     .then((result) => {  setSkills(result.results)  })
     .catch((error) => console.error(error))
@@ -27,7 +119,7 @@ const Portfolio = () => {
       {projects.map((project) => {
         return(
           <div className="shadow-lg rounded-lg translate-y-10 opacity-0 duration-300" key={project.id}>
-          <div className="relative h-[300px]" style={{overflow: "hidden"}}>
+          <div className="relative h-[300px]">
             <img src={project.image} alt="project" className='w-full rounded-t-lg' />
             <a href={project.link} target='_blank' rel="noreferrer" className="text-[var(--color-primary)] duration-300 border border-[var(--heading-color)] hover:text-[var(--heading-color)] hover:bg-[var(--color-primary)] w-8 h-8 absolute top-2 right-2 bg-[var(--heading-color)] flex justify-center items-center rounded shadow-2xl">
             <i className="fa-solid fa-eye"></i>
